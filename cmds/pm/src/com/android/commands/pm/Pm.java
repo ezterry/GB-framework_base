35df9050b8b7119e7711823872e1a085b1528556
Firerat <firer4t@googlemail.com>
2010-12-30 13:12:21 +0000

    re-order error output of pm error is now printed below Usage output much easier to see error, no need to 'scroll up'

--- cmds/pm/src/com/android/commands/pm/Pm.java
+++ cmds/pm/src/com/android/commands/pm/Pm.java
@@ -144,10 +144,10 @@ public final class Pm {
             }
         } finally {
             if (validCommand == false) {
+                showUsage();
                 if (op != null) {
                     System.err.println("Error: unknown command '" + op + "'");
                 }
-                showUsage();
             }
         }
     }
@@ -165,8 +165,8 @@ public final class Pm {
     private void runList() {
         String type = nextArg();
         if (type == null) {
-            System.err.println("Error: didn't specify type of data to list");
             showUsage();
+            System.err.println("Error: didn't specify type of data to list");
             return;
         }
         if ("package".equals(type) || "packages".equals(type)) {
@@ -182,8 +182,8 @@ public final class Pm {
         } else if ("instrumentation".equals(type)) {
             runListInstrumentation();
         } else {
-            System.err.println("Error: unknown list type '" + type + "'");
             showUsage();
+            System.err.println("Error: unknown list type '" + type + "'");
         }
     }
 
@@ -209,14 +209,14 @@ public final class Pm {
                 } else if (opt.equals("-u")) {
                     getFlags |= PackageManager.GET_UNINSTALLED_PACKAGES;
                 } else {
-                    System.err.println("Error: Unknown option: " + opt);
                     showUsage();
+                    System.err.println("Error: Unknown option: " + opt);
                     return;
                 }
             }
         } catch (RuntimeException ex) {
-            System.err.println("Error: " + ex.toString());
             showUsage();
+            System.err.println("Error: " + ex.toString());
             return;
         }
 
@@ -339,14 +339,14 @@ public final class Pm {
                 } else if (opt.charAt(0) != '-') {
                     targetPackage = opt;
                 } else {
-                    System.err.println("Error: Unknown option: " + opt);
                     showUsage();
+                    System.err.println("Error: Unknown option: " + opt);
                     return;
                 }
             }
         } catch (RuntimeException ex) {
-            System.err.println("Error: " + ex.toString());
             showUsage();
+            System.err.println("Error: " + ex.toString());
             return;
         }
 
@@ -435,8 +435,8 @@ public final class Pm {
                 } else if (opt.equals("-d")) {
                     dangerousOnly = true;
                 } else {
-                    System.err.println("Error: Unknown option: " + opt);
                     showUsage();
+                    System.err.println("Error: Unknown option: " + opt);
                     return;
                 }
             }
@@ -597,8 +597,8 @@ public final class Pm {
     private void runPath() {
         String pkg = nextArg();
         if (pkg == null) {
-            System.err.println("Error: no package specified");
             showUsage();
+            System.err.println("Error: no package specified");
             return;
         }
         displayPackageFilePath(pkg);
@@ -655,21 +655,21 @@ public final class Pm {
 
         String arg = nextArg();
         if (arg == null) {
-            System.err.println("Error: no install location specified.");
             showUsage();
+            System.err.println("Error: no install location specified.");
             return;
         }
         try {
             loc = Integer.parseInt(arg);
         } catch (NumberFormatException e) {
-            System.err.println("Error: install location has to be a number.");
             showUsage();
+            System.err.println("Error: install location has to be a number.");
             return;
         }
         try {
             if (!mPm.setInstallLocation(loc)) {
-                System.err.println("Error: install location has to be a number.");
                 showUsage();
+                System.err.println("Error: install location has to be a number.");
             }
         } catch (RemoteException e) {
             System.err.println(e.toString());
@@ -708,8 +708,8 @@ public final class Pm {
             } else if (opt.equals("-i")) {
                 installerPackageName = nextOptionData();
                 if (installerPackageName == null) {
-                    System.err.println("Error: no value specified for -i");
                     showUsage();
+                    System.err.println("Error: no value specified for -i");
                     return;
                 }
             } else if (opt.equals("-t")) {
@@ -721,8 +721,8 @@ public final class Pm {
                 // Override if -s option is specified.
                 installFlags |= PackageManager.INSTALL_INTERNAL;
             } else {
-                System.err.println("Error: Unknown option: " + opt);
                 showUsage();
+                System.err.println("Error: Unknown option: " + opt);
                 return;
             }
         }
@@ -730,8 +730,8 @@ public final class Pm {
         String apkFilePath = nextArg();
         System.err.println("\tpkg: " + apkFilePath);
         if (apkFilePath == null) {
-            System.err.println("Error: no package specified");
             showUsage();
+            System.err.println("Error: no package specified");
             return;
         }
 
@@ -784,8 +784,8 @@ public final class Pm {
 
         String pkg = nextArg();
         if (pkg == null) {
-            System.err.println("Error: no package specified");
             showUsage();
+            System.err.println("Error: no package specified");
             return;
         }
         boolean result = deletePackage(pkg, unInstallFlags);
@@ -880,8 +880,8 @@ public final class Pm {
     private void runSetEnabledSetting(int state) {
         String pkg = nextArg();
         if (pkg == null) {
-            System.err.println("Error: no package or component specified");
             showUsage();
+            System.err.println("Error: no package or component specified");
             return;
         }
         ComponentName cn = ComponentName.unflattenFromString(pkg);
@@ -1057,5 +1057,6 @@ public final class Pm {
         System.err.println("  0 [auto]: Let system decide the best location");
         System.err.println("  1 [internal]: Install on internal device storage");
         System.err.println("  2 [external]: Install on external media");
+        System.err.println("");
     }
 }
